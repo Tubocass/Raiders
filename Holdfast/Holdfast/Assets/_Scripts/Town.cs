7dc7d5c323d2ccea2a015795572a4506e91d0d62
Tubocass <tubocass@gmail.com>
2020-08-02 20:10:03 -0400

    Added a clean new project to screw around in.

---
+++ Holdfast/Holdfast/Assets/_Scripts/Town.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Town : MonoBehaviour
+{
+   [SerializeField] TownInfo myTownInfo;
+
+    // Use this for initialization
+    void Start()
+    {
+        Dictionary<string, string> info = myTownInfo.getTownInfo();
+        Dictionary<string,string>.KeyCollection keys = info.Keys;
+        foreach(string key in keys)
+        {
+            string value;
+            Debug.Log(info.TryGetValue(key, out value) ? key +": "+ value : "");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Holdfast/Holdfast/Assets/_Scripts/TownInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/TownInfo", order = 1)]
+public class TownInfo: ScriptableObject
+{
+    [SerializeField] string townName;
+    [SerializeField] int population, gold, food;
+    Dictionary<string,string> keyValues = new Dictionary<string, string>();
+
+    public Dictionary<string, string> getTownInfo()
+    {
+        keyValues.Add("townName", this.townName);
+        keyValues.Add("population", this.population.ToString());
+        keyValues.Add("gold", this.gold.ToString());
+        keyValues.Add("food", this.food.ToString());
+
+        return keyValues;
+    }
+}
